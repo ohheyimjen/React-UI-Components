a5a11fd55f54c7770af37ba56bcd4524f157da33
ohheyimjen <jendority@gmail.com>
2019-03-25 14:31:49 -0700

    Filled out HeaderTitle.js, but it isn't correct

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+
+    return (
+        <div>
+            <span>
+            <header>Lambda School</header>
+            <h4>@LambdaSchool</h4>
+            <h4>March 25</h4>
+            </span>
+        </div>
+    );
+};
+
+export default HeaderTitle;
